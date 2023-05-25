a2639ccd3a1e3cd4a8bf765d1cafb470dbd27eb0
yudaeon <daeon03050@gmail.com>
2023-05-25 23:22:06 +0900

    feat : spring-basic chap01~02 commits

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,29 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+    model.addAttribute("data","spring!!");
+    return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = false)String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-templete";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //바디 부분에 직접 넣겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //"hello spring"
+    }
+}
