1ed71f3e71c1a7d558f4ce6812d548cfb88a3efc
yudaeon <daeon03050@gmail.com>
2023-05-26 01:33:09 +0900

    feat : spring-basic commits

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UrlPathHelper;
 
 @Controller
 public class HelloController {
@@ -25,5 +26,25 @@ public class HelloController {
     @ResponseBody //바디 부분에 직접 넣겠다.
     public String helloString(@RequestParam("name") String name){
         return "hello " + name; //"hello spring"
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name); //parameter로 넘김
+        return hello;
+
+    }
+
+    static class Hello{
+        private String name;
+        //getter, setter 만들기
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
